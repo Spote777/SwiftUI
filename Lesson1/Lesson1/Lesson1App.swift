d9d582716a65e761bda7c8807252ec383af4c0fe
Spote777 <66115092+Spote777@users.noreply.github.com>
2021-10-23 15:19:08 +0300

    SwiftUI Lessons

---
+++ Lesson1/Lesson1/Lesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson1App.swift
+//  Lesson1
+//
+//  Created by Павел Заруцков on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Lesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
